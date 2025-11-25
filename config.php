89ff9744e36fde8661a5c2c5cd49b90a78893d35
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-11-25 12:51:35 +0000

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,10 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'company');
+define('DB_PORT', '');
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
