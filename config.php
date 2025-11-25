1120c6f07f1df17ef70686e68154e893272ebf8e
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-11-25 12:56:14 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'company');
-define('DB_PORT', '');
+define('DB_SERVER', 'mysql-18fa14a0-waltersahndre-5c0e.g.aivencloud.com');
+define('DB_USERNAME', 'avnadmin');
+define('DB_PASSWORD', 'AVNS_JALLZ2rTa2mMGIoPdv6');
+define('DB_NAME', 'defaultdb');
+define('DB_PORT', '19155');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
